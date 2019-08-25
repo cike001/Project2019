7bcb10308cec757cbed82f4a87757be073aa8af5
cike001 <mini_cike@163.com>
2019-08-25 18:52:36 +0800

    add

---
+++ docker/src/main/java/net/gwy/docker/controller/DockerController.java
@@ -0,0 +1,20 @@
+package net.gwy.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gwy
+ * @time 2019-08-25 18:17
+ * @desc TODO
+ **/
+@RestController
+@RequestMapping("/docker")
+public class DockerController {
+
+    @RequestMapping("/info")
+    public String info(){
+        return "hello docker";
+    }
+
+}
